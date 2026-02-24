9419ff8690292cd09ec8c6c9675b0dbaeae914c5
Hurayrah <hurayrah92@gmail.com>
2026-02-24 00:20:55 -0500

    testing

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, C!\n");
+    //hello
+    int number = 10;
+    char x = 'b';
+    double doub = 10.2345234;
+    printf("number is equal to %d\n", number);
+    printf("x is a character: %c\n", x);
+    printf("doub is equal to %f\n", doub);
+    printf("hi\n");
     return 0;
 }
